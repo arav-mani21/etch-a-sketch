2ad2bdc1c16d57717221969c84ba2d8ab5fade80
Aravind Manivannan <192905623+arav-mani21@users.noreply.github.com>
2025-02-26 18:44:34 -0500

    script.js now centers around two main functions, loadGrid() and updateSize() loadGrid() loads the grid by populating the main div container with a grid of small divs updateSize() requests user for new grid size and calls loadGrid() with the new size as input

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let squaresPerSide = 16
+document.addEventListener("DOMContentLoaded",function(){loadGrid(16)})
+
+function loadGrid(squaresPerSide){
+    let color = [Math.random()*100,Math.random()*100,Math.random()*100]
+    let rgb = `rgb(${color[0]}%,${color[1]}%,${color[2]}%)`
+    
+    const containerDiv = document.querySelector('#container')
+    containerDiv.style.borderColor = rgb
+
+    for(let i = 0; i < squaresPerSide; i++){
+        for(let i = 0; i < squaresPerSide; i++){
+            const gridItem = document.createElement("div")
+            gridItem.classList.toggle("grid-item")
+
+            let length = (100/squaresPerSide) + "%"
+            gridItem.style.width = length
+            gridItem.style.height = length
+
+            gridItem.addEventListener("mouseover",() => gridItem.style.backgroundColor = rgb)
+            containerDiv.appendChild(gridItem)
+        }
+    }
+}
+
+function updateSize(){
+    let newSize = prompt("How many squares per side would you like in your new grid?",16)
+    document.querySelector("#container").innerHTML = ""
+    loadGrid(newSize)
+}
